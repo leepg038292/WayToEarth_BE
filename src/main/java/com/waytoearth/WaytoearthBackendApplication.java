6afbacccdb4da1e25c541d5c34779a4a0ba8f6c8
hskhsmm <hskhsmm@naver.com>
2025-07-28 17:12:00 +0900

    feat: 기초 간단 설정

---
+++ src/main/java/com/waytoearth/WaytoearthBackendApplication.java
@@ -0,0 +1,13 @@
+package com.waytoearth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaytoearthBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaytoearthBackendApplication.class, args);
+	}
+
+}
