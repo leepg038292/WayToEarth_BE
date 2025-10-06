74a5e3e1243e6288f1c2be5c3927312e40d5f0fd
hskhsmm <hskhsmm@naver.com>
2025-10-06 17:35:19 +0900

    fix: @EnableScheduling 중복 제거

--- src/main/java/com/waytoearth/config/scheduling/ChatSchedulerConfig.java
+++ src/main/java/com/waytoearth/config/scheduling/ChatSchedulerConfig.java
@@ -4,11 +4,9 @@ import com.waytoearth.util.ChatRateLimiter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
-@EnableScheduling
 @RequiredArgsConstructor
 @Slf4j
 public class ChatSchedulerConfig {
