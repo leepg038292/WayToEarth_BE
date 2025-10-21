9364269502aa96e56368fb3b0ec83adcae2c4153
hskhsmm <hskhsmm@naver.com>
2025-10-21 23:59:33 +0900

    fix: crew 응답 dto cdn 주소로 변경

--- src/main/java/com/waytoearth/dto/response/crew/CrewListResponse.java
+++ src/main/java/com/waytoearth/dto/response/crew/CrewListResponse.java
@@ -1,6 +1,7 @@
 package com.waytoearth.dto.response.crew;
 
 import com.waytoearth.entity.crew.CrewEntity;
+import com.waytoearth.service.file.FileService;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
