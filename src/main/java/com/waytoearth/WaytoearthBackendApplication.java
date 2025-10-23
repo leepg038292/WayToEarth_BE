55fdf7769a37dcf6f1a4528850e037e260d9104c
hskhsmm <hskhsmm@naver.com>
2025-10-23 20:29:35 +0900

    feat: 명시적 EnableRedisRepositories 추가

--- src/main/java/com/waytoearth/WaytoearthBackendApplication.java
+++ src/main/java/com/waytoearth/WaytoearthBackendApplication.java
@@ -2,10 +2,12 @@ package com.waytoearth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableRetry
+@EnableRedisRepositories
 public class WaytoearthBackendApplication {
 
 	public static void main(String[] args) {
