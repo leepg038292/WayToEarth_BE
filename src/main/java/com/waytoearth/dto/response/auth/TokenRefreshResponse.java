cb12170577b047091e9f5a0462e69a626ab8c817
hskhsmm <hskhsmm@naver.com>
2025-10-23 18:19:24 +0900

    feat:토큰 재발급 응답 DTO 추가

---
+++ src/main/java/com/waytoearth/dto/response/auth/TokenRefreshResponse.java
@@ -0,0 +1,18 @@
+package com.waytoearth.dto.response.auth;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Schema(description = "토큰 재발급 응답")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenRefreshResponse {
+
+    @Schema(description = "새로 발급된 액세스 토큰 (15분)", example = "eyJhbGciOiJIUzI1NiJ9...")
+    private String accessToken;
+}
